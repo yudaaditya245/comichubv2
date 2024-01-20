7f7f68c4a79ec129aed949dcdd37d9afb17f2178
yudaaditya245 <yudaaditya245@gmail.com>
2024-01-20 15:11:52 +0700

    setup basic router

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import About from "../pages/About.jsx";
+import LayoutWithNav from "../layout/LayoutWithNav.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route element={<LayoutWithNav />}>
+      <Route path="/" element={<App />} />
+      <Route path="/about" element={<About />} />
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
